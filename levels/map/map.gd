2d58099cea10bf339b037ed2e8361a94a48be799
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-07-22 16:42:14 +0300

    add map

--- levels/map/map.gd
+++ levels/map/map.gd
@@ -1,23 +1,32 @@
-extends Node2D
+extends Control
 
+var paused = false
+func closeMap():
+	paused = false
+	visible = false
+	
+func openMap():
+	paused = true
+	visible = true
 
-
+func input_map():
+	if Input.is_action_just_pressed("map"):
+		if paused:
+			closeMap()
+		else:
+			openMap()
+		
+func _process(delta):
+	input_map()
 
 func _on_ajloun_pressed():
 	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://levels/ajloun/ajloun.tscn")
 	
-
-
-
 func _on_jarash_pressed():
 	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://levels/jarash/mainJarashScene.tscn")
 	
-	
-	
-
-
 func _on_petra_pressed():
 	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://levels/Petra/mainPetra.tscn")
