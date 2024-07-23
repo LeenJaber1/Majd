9967ccb8895685ac5e24efdec694815955897526
Tala <talaqudah86@gmail.com>
2024-07-23 09:44:42 +0300

    stashed

---
+++ levels/ajloun/passwordPuzzle/passwordPuzzle.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _on_submit_button_button_down():
+	var word = "جلعاد"
+	var lineEdits = [$firstLineEdit, $secondLineEdit, $thirdLineEdit, $fourthLineEdit, $fifthLineEdit]
+
+	for index in 5:
+		if word[index] != lineEdits[index].get_text():
+			$wrongAudio.play()
+			break
+		if index == 4:
+			$correctAudio.play()
+			await $correctAudio.finished
+			get_tree().change_scene_to_file("res://levels/ajloun/mosiacPuzzle/mosiacPuzzle.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://levels/ajloun/ajloun.tscn")
